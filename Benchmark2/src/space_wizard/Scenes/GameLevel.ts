e7739d6d5ac36ed6a21f4f121551a3eb21136f03
Kevin <Zheng@DESKTOP-091D5CM>
2021-04-18 00:56:04 -0400

    cant figure out how to make bar move from right to left, brain is bad

--- Benchmark2/src/space_wizard/Scenes/GameLevel.ts
+++ Benchmark2/src/space_wizard/Scenes/GameLevel.ts
@@ -8,6 +8,7 @@ import Color from "../../Wolfie2D/Utils/Color";
 import Input from "../../Wolfie2D/Input/Input";
 import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
 import Graphic from "../../Wolfie2D/Nodes/Graphic";
+import CanvasNode from "../../Wolfie2D/Nodes/CanvasNode";
 
 
 export default class GameLevel extends Scene{
@@ -17,10 +18,11 @@ export default class GameLevel extends Scene{
 
     private healthBar:Graphic;  //I dont think graphic works with healthbars
     private manaBar:Graphic; //not sure
+    private testbar:Sprite;
 
     //these number should from the player character, not sure how to handle it
     private health:number=100;
-    private mana:number=100; 
+    private mana:number=99; 
     private minBar:Vec2;//position of 0 in health bar
     private maxBar:Vec2;//position of 100 in health bar
 
@@ -33,7 +35,7 @@ export default class GameLevel extends Scene{
     startScene(){
         this.addUILayer("UI");
         this.drawHealthAndMana("UI");
-        this.updateHealthAndMana(-50,0);
+        this.updateHealthAndMana(-50,-20);
     }
 
     //you know 
@@ -57,36 +59,42 @@ export default class GameLevel extends Scene{
     }
 
     //draw health and mana bar for setup
+    //maybe we can have a number in the middle, idk
     drawHealthAndMana(layer:string){
-        this.minBar = new Vec2(this.x*0.1, this.y*0.95-15);
-        this.maxBar = new Vec2(this.x*0.9, this.y*0.95-15); //this is really jank, fix it later by using var 
+        //needs to be more down?
         this.healthBar = this.add.graphic(GraphicType.RECT,layer,{position: new Vec2(this.x*0.5, this.y*0.95-15), size: new Vec2(this.x*0.8, 10)});
         this.manaBar = this.add.graphic(GraphicType.RECT,layer,{position: new Vec2(this.x*0.5, this.y*0.95), size: new Vec2(this.x*0.8, 10)});
         this.manaBar.setColor(Color.BLUE);
-        this.healthBar.
     }
 
     //draw health and mana bars as X/100
     //caps at 100%
     //idk how to make a healthbar, its not changing.
     updateHealthAndMana(deltaHealth:number,deltaMana:number){
-        // this.health= this.health+deltaHealth;
-        // this.mana = this.mana+deltaMana;
+        this.health= this.health+deltaHealth;
+        this.mana = this.mana+deltaMana;
 
-        // let percentHealth = this.health/100;
-        // let percentMana = this.mana/100;
+        let percentHealth = this.health/100;
+        let percentMana = this.mana/100;
 
-        // //cap to 100 percent
-        // percentHealth>1 ? percentHealth=1 : percentHealth;
-        // percentMana>1 ? percentMana=1 :percentMana; 
+        //cap to 100 percent
+        percentHealth>1 ? percentHealth=1 : percentHealth;
+        percentMana>1 ? percentMana=1 :percentMana; 
 
-        // //waitt.....
-        // // I can just change the size of the healthbar, and then shift the position by the size
-        // let newX = this.x * percentHealth;
-        // this.healthBar.position.x = newX;
-        // this.healthBar.scaleX = percentHealth;
-        // this.healthBar = this.add.graphic(GraphicType.RECT,"UI",{position: new Vec2(newX, this.y*0.95-20), size: new Vec2(percentHealth, 10)});
-        // //just want to move the
+        //uh, how do I change the healthbar
+        //the math is a little wonky on this :/
+        this.healthBar.destroy();
+        this.manaBar.destroy();
+        let center = this.viewport.getCenter();
+        let newX = center.x - (this.x*0.8*percentHealth*0.5);
+        let newSize = this.x*0.8*percentHealth;
+        this.healthBar = this.add.graphic(GraphicType.RECT,"UI",{position: new Vec2(newX, this.y*0.95-15), size: new Vec2(newSize, 10)});
+
+        newSize = this.x*0.8*percentMana;
+        newX = center.x - (this.x*0.8*percentMana*0.5);
+        this.manaBar = this.add.graphic(GraphicType.RECT,"UI",{position: new Vec2(newX, this.y*0.95), size: new Vec2(newSize, 10)});
+        this.manaBar.setColor(Color.BLUE);
+        
     }
 
     //render spells casted to cooldown, and reduce cooldown for all spells?
