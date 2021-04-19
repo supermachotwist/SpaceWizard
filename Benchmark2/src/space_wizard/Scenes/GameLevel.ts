f0bf4117bbc0a678e3975dd0586f24f86cd34d61
Kevin <Zheng@DESKTOP-091D5CM>
2021-04-19 06:26:37 -0400

    commit for pull

--- Benchmark2/src/space_wizard/Scenes/GameLevel.ts
+++ Benchmark2/src/space_wizard/Scenes/GameLevel.ts
@@ -2,110 +2,6 @@ import PlayerController from "../AI/PlayerController";
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
 import Scene from "../../Wolfie2D/Scene/Scene";
-<<<<<<< HEAD
-import { GraphicType } from "../../Wolfie2D/Nodes/Graphics/GraphicTypes";
-import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
-import Color from "../../Wolfie2D/Utils/Color";
-import Input from "../../Wolfie2D/Input/Input";
-import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
-import Graphic from "../../Wolfie2D/Nodes/Graphic";
-import CanvasNode from "../../Wolfie2D/Nodes/CanvasNode";
-
-
-export default class GameLevel extends Scene{
-    //big x and big y for % of screen
-    private x = this.viewport.getCenter().x*2;
-    private y = this.viewport.getCenter().y*2;
-
-    private healthBar:Graphic;  //I dont think graphic works with healthbars
-    private manaBar:Graphic; //not sure
-    private testbar:Sprite;
-
-    //these number should from the player character, not sure how to handle it
-    private health:number=100;
-    private mana:number=99; 
-    private minBar:Vec2;//position of 0 in health bar
-    private maxBar:Vec2;//position of 100 in health bar
-
-    //something every level needs to load?
-    loadScene(){
-        this.loadUIFiles();
-    }
-
-    //something everylevel need to initiate, like ui
-    startScene(){
-        this.addUILayer("UI");
-        this.drawHealthAndMana("UI");
-        this.updateHealthAndMana(-50,-20);
-    }
-
-    //you know 
-    updateScene(){
-
-    }
-
-    //UI Stuff
-    //Just Health and Mana
-    //Cooldown System
-    //that line from sprite to cursor when button is pressed
-
-    //do need to connect it to the player character
-
-    //loads the stuff for the UI
-    loadUIFiles(){
-        //health and mana
-
-        //spells
-        //spell icon, line assist?
-    }
-
-    //draw health and mana bar for setup
-    //maybe we can have a number in the middle, idk
-    drawHealthAndMana(layer:string){
-        //needs to be more down?
-        this.healthBar = this.add.graphic(GraphicType.RECT,layer,{position: new Vec2(this.x*0.5, this.y*0.95-15), size: new Vec2(this.x*0.8, 10)});
-        this.manaBar = this.add.graphic(GraphicType.RECT,layer,{position: new Vec2(this.x*0.5, this.y*0.95), size: new Vec2(this.x*0.8, 10)});
-        this.manaBar.setColor(Color.BLUE);
-    }
-
-    //draw health and mana bars as X/100
-    //caps at 100%
-    //idk how to make a healthbar, its not changing.
-    updateHealthAndMana(deltaHealth:number,deltaMana:number){
-        this.health= this.health+deltaHealth;
-        this.mana = this.mana+deltaMana;
-
-        let percentHealth = this.health/100;
-        let percentMana = this.mana/100;
-
-        //cap to 100 percent
-        percentHealth>1 ? percentHealth=1 : percentHealth;
-        percentMana>1 ? percentMana=1 :percentMana; 
-
-        //uh, how do I change the healthbar
-        //the math is a little wonky on this :/
-        this.healthBar.destroy();
-        this.manaBar.destroy();
-        let center = this.viewport.getCenter();
-        let newX = center.x - (this.x*0.8*percentHealth*0.5);
-        let newSize = this.x*0.8*percentHealth;
-        this.healthBar = this.add.graphic(GraphicType.RECT,"UI",{position: new Vec2(newX, this.y*0.95-15), size: new Vec2(newSize, 10)});
-
-        newSize = this.x*0.8*percentMana;
-        newX = center.x - (this.x*0.8*percentMana*0.5);
-        this.manaBar = this.add.graphic(GraphicType.RECT,"UI",{position: new Vec2(newX, this.y*0.95), size: new Vec2(newSize, 10)});
-        this.manaBar.setColor(Color.BLUE);
-        
-    }
-
-    //render spells casted to cooldown, and reduce cooldown for all spells?
-    //not too sure how to reduce cooldown, maybe a percentage kind of thing
-    //maybe draw the assist line here
-    drawSpell(spell1:boolean){
-
-    }
-
-=======
 import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
 import SpellManager from "../GameSystems/Spells/SpellManager";
 import Spell from "../GameSystems/Spells/Spell";
@@ -164,9 +60,7 @@ export default class GameLevel extends Scene {
 
         // Initialize array of towers
         this.towers = new Array();
-
         this.initializePlayer();
-
         this.spawnTowers();
     }
 
@@ -232,5 +126,11 @@ export default class GameLevel extends Scene {
             }
         }        
     }
->>>>>>> 7ebfca5b745a380641f26a7579c1e011ac1dbb2c
+
+    //here lies my healthbar D:
+    //um, its just two rectange, so I am sure it just fine. 
+
+
+
 }
+
--- Benchmark2/src/space_wizard/Scenes/Level1.ts
+++ Benchmark2/src/space_wizard/Scenes/Level1.ts
@@ -19,59 +19,57 @@ import GameLevel from "./Gamelevel";
 
 
 export default class Level1 extends GameLevel {
-    // The player
-    private player: AnimatedSprite;
-    // Logo
-    private logo: Sprite;
 
-    loadScene(): void {
-        super.loadScene(); //possibly shouldn't be here because we can choose the needed asset moving forward
-        this.load.spritesheet("player", "space_wizard_assets/spritesheets/WizardPlayer.json");
-        this.load.image("logo", "space_wizard_assets/images/Space Wizard Logo.png");
+    //dont think we need this rn
 
-        this.loadUIFiles();
-    }
+    // loadScene(): void {
+    //     super.loadScene(); //possibly shouldn't be here because we can choose the needed asset moving forward
+    //     this.load.spritesheet("player", "space_wizard_assets/spritesheets/WizardPlayer.json");
+    //     this.load.image("logo", "space_wizard_assets/images/Space Wizard Logo.png");
 
-    // startScene() is where you should build any game objects you wish to have in your scene,
-    // or where you should initialize any other things you will need in your scene
-    // Once again, this occurs strictly after loadScene(), so anything you loaded there will be available
-    startScene(): void {
-        super.startScene();
+    //     this.loadUIFiles();
+    // }
 
-        // Create any game objects here. For example, to add the sprite we previously loaded:
+    // // startScene() is where you should build any game objects you wish to have in your scene,
+    // // or where you should initialize any other things you will need in your scene
+    // // Once again, this occurs strictly after loadScene(), so anything you loaded there will be available
+    // startScene(): void {
+    //     super.startScene();
 
-        // First, create a layer for it to go on
-        this.addLayer("primary");
+    //     // Create any game objects here. For example, to add the sprite we previously loaded:
 
-        // The first argument is the key we specified in "this.load.image"
-        // The second argument is the name of the layer
-        this.logo = this.add.sprite("logo", "primary");
+    //     // First, create a layer for it to go on
+    //     this.addLayer("primary");
 
-        // Now, let's make sure our logo is in a good position
-        let center = this.viewport.getCenter();
-        this.logo.position.set(center.x, center.y);
+    //     // The first argument is the key we specified in "this.load.image"
+    //     // The second argument is the name of the layer
+    //     this.logo = this.add.sprite("logo", "primary");
 
-        this.initializePlayer();
+    //     // Now, let's make sure our logo is in a good position
+    //     let center = this.viewport.getCenter();
+    //     this.logo.position.set(center.x, center.y);
+
+    //     this.initializePlayer();
 
         
-    }
+    // }
 
-    initializePlayer(): void {
-        // Get center of viewport
-        let center = this.viewport.getCenter();
+    // initializePlayer(): void {
+    //     // Get center of viewport
+    //     let center = this.viewport.getCenter();
 
-        // Create the player
-        this.player = this.add.animatedSprite("player", "primary");
-        this.player.position.set(center.x, center.y + 100);
-        this.player.addAI(PlayerController,{});
+    //     // Create the player
+    //     this.player = this.add.animatedSprite("player", "primary");
+    //     this.player.position.set(center.x, center.y + 100);
+    //     this.player.addAI(PlayerController,{});
 
-        // Start player is idle animation on loop
-        this.player.animation.play("IDLE", true);
-    }
+    //     // Start player is idle animation on loop
+    //     this.player.animation.play("IDLE", true);
+    // }
 
-    updateScene(){
-        super.updateScene();
-    }
+    // updateScene(){
+    //     super.updateScene();
+    // }
 
 
 
