38e44ee2e71697ceb8a341c25b9adb18461c5e92
Kevin <Zheng@DESKTOP-091D5CM>
2021-04-19 14:25:22 -0400

    removed bundle.js from tracked files

--- Benchmark2/src/space_wizard/Scenes/GameLevel.ts
+++ Benchmark2/src/space_wizard/Scenes/GameLevel.ts
@@ -11,6 +11,13 @@ import ExplosionTower from "../GameSystems/Towers/ExplosionTower";
 import ForkTower from "../GameSystems/Towers/ForkTower";
 import PierceTower from "../GameSystems/Towers/PierceTower";
 import Circle from "../../Wolfie2D/DataTypes/Shapes/Circle";
+import Button from "../../Wolfie2D/Nodes/UIElements/Button";
+import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
+import Color from "../../Wolfie2D/Utils/Color";
+import GameLoop from "../../Wolfie2D/Loop/GameLoop";
+import { GraphicType } from "../../Wolfie2D/Nodes/Graphics/GraphicTypes";
+import Rect from "../../Wolfie2D/Nodes/Graphics/Rect";
+import UIElement from "../../Wolfie2D/Nodes/UIElement";
 
 
 
@@ -46,6 +53,7 @@ export default class GameLevel extends Scene {
     startScene(): void {
 
         // First, create a layer for it to go on
+        this.addLayer("settingMenu",100);
         this.addLayer("primary", 50);
         this.addLayer("background", 0);
 
@@ -56,15 +64,11 @@ export default class GameLevel extends Scene {
         // Now, let's make sure our logo is in a good position
         let center = this.viewport.getCenter();
         this.logo.position.set(center.x, center.y);
+        this.settingButton();
 
 
         // Initialize array of towers
         this.towers = new Array();
-<<<<<<< HEAD
-        this.initializePlayer();
-=======
-
->>>>>>> babe980135dc06d99fef7c555ddc6c20e2ab28c8
         this.spawnTowers();
 
         this.initializePlayer();
@@ -132,15 +136,42 @@ export default class GameLevel extends Scene {
             }
         }        
     }
-<<<<<<< HEAD
 
-    //here lies my healthbar D:
-    //um, its just two rectange, so I am sure it just fine. 
+    //replace button with a image at a later date
+    settingButton(){
+        let midpoint = this.viewport.getCenter();
+        let playButton = <Button> this.add.uiElement(UIElementType.BUTTON,"settingMenu",{ position:new Vec2(midpoint.x,midpoint.y+120),text:"Setting"});
+        playButton.backgroundColor = Color.BLACK;
+        playButton.borderColor = Color.BLACK;
+        playButton.borderRadius = 10;
+        playButton.setPadding(new Vec2(50, 10));
+        playButton.font = "PixelSimple";
+        playButton.onClick = () => {
+            this.createSettingMenu();
+        }
+        return playButton;
+    }
 
+    //wait, how to pause game????
+    
+    createSettingMenu(){
+        let center = this.viewport.getCenter();
 
+        let musicSliderLabel = <UIElement> this.add.uiElement(UIElementType.LABEL,"settingMenu",{position: new Vec2(center.x-400,center.y),text:"muusic"});
+        let musicSlider = <UIElement> this.add.uiElement(UIElementType.SLIDER,"settingMenu",{position: new Vec2(center.x-100,center.y)});
+        musicSlider.backgroundColor = Color.GREEN; //there might be a bug with the slider, idk
+        musicSliderLabel.backgroundColor=Color.BLUE;
+        // let settingBackground = <Rect>this.add.graphic(GraphicType.RECT,"settingMenu",{position:new Vec2(center.x,center.y),size:new Vec2(1000,500)});
+        // settingBackground.color = Color.WHITE;
 
+        let exitButton = <UIElement> this.add.uiElement(UIElementType.BUTTON,"settingMenu",{position:new Vec2(center.x,center.y+200),text:"exit"});
+        exitButton.onClick = () =>{
+            musicSlider.destroy();
+            exitButton.destroy();
+            musicSliderLabel.destroy();
+          //  settingBackground.destroy();
+            console.log("Exit Setting");
+        }
+    }
+    
 }
-
-=======
-}
->>>>>>> babe980135dc06d99fef7c555ddc6c20e2ab28c8
--- Benchmark2/src/space_wizard/Scenes/MainMenu.ts
+++ Benchmark2/src/space_wizard/Scenes/MainMenu.ts
@@ -1,60 +1,45 @@
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import { GraphicType } from "../../Wolfie2D/Nodes/Graphics/GraphicTypes";
+import Rect from "../../Wolfie2D/Nodes/Graphics/Rect";
 import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
 import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
+import UIElement from "../../Wolfie2D/Nodes/UIElement";
 import Button from "../../Wolfie2D/Nodes/UIElements/Button";
 import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
 import Scene from "../../Wolfie2D/Scene/Scene";
 import Color from "../../Wolfie2D/Utils/Color";
+import Level1 from "./Level1";
 
 
 export default class MainMenu extends Scene {
     private background:Sprite;
     animatedSprite: AnimatedSprite;
+    setting:boolean;
 
-    //its taking a while to load because of the mp3, which like 4 minutes long
     loadScene():void{
         this.load.image("mainMenuBackground","space_wizard_assets/images/MainMenu.png");
         //this.load.audio("mainMenuMusic","space_wizard_assets/sounds/PlaceholderMusic.mp3");
     }
 
     startScene():void{
-        this.addUILayer("mainMenu");
-        this.background = this.add.sprite("mainMenuBackground","mainMenu");
+        this.addLayer("mainMenu",1);
+        this.addLayer("mainMenuBackground",0);
+        this.addLayer("settingMenuBackGround",2);
+        this.addLayer("settingMenu",3);
+        this.background = this.add.sprite("mainMenuBackground","mainMenuBackground");
 
         let center = this.viewport.getCenter();
         this.background.position.set(center.x,center.y);
         this.viewport.setFocus(this.viewport.getHalfSize());
 
-        //this.emitter.fireEvent(GameEventType.PLAY_MUSIC,{key:"mainMenuMusic",loop:true,holdReference:true});
-        
-        //make button!
-        //make left right button(also work with arrrow/wasd keys)
-        //make play button, which changes its content when changing level,also, when 
-        //make setting buttons, which shows setting
-        //make help buttons, which shows the help page
-
         let midpoint = this.viewport.getHalfSize();
         //Play Button
         //Click on this will start the level
-        let playButton = <Button> this.add.uiElement(UIElementType.BUTTON,"mainMenu",{ position:new Vec2(midpoint.x,midpoint.y),text:"Level 1"});
-        playButton.backgroundColor = Color.BLACK;
-        playButton.borderColor = Color.BLACK;
-        playButton.borderRadius = 10;
-        playButton.setPadding(new Vec2(50, 10));
-        playButton.font = "PixelSimple";
-        playButton.onClick = () => {
-            console.log("Play!");
-        }
-
-        //Setting Button
-        //Clicking on this will open the setting menu
-        //how do I make the setting menu?
-        //new layers I guess
-        let settingButton = <Button> this.add.uiElement(UIElementType.BUTTON,"mainMenu",{
-                        position:new Vec2()
-        })
-
+        this.makePlayButton();
+        this.makeHelpButton();
+        this.makeSettingButton();
+    
         
     }
 
@@ -66,5 +51,94 @@ export default class MainMenu extends Scene {
         //unload music here
         //this.emitter.fireEvent(GameEventType.STOP_SOUND,{key:"mainMenu"});
 
+    }
+
+    makePlayButton():Button{
+        let midpoint = this.viewport.getCenter();
+        let playButton = <Button> this.add.uiElement(UIElementType.BUTTON,"mainMenu",{ position:new Vec2(midpoint.x,midpoint.y),text:"Demo Level"});
+        playButton.backgroundColor = Color.BLACK;
+        playButton.borderColor = Color.BLACK;
+        playButton.borderRadius = 10;
+        playButton.setPadding(new Vec2(50, 10));
+        playButton.font = "PixelSimple";
+        playButton.onClick = () => {
+            console.log("Activated Play Button");
+            this.setting?{}:this.sceneManager.changeToScene(Level1,{},{});
+        }
+        return playButton;
+    }
+
+    makeHelpButton():Button{
+        let midpoint = this.viewport.getCenter();
+        let playButton = <Button> this.add.uiElement(UIElementType.BUTTON,"mainMenu",{ position:new Vec2(midpoint.x,midpoint.y+60),text:"Help"});
+        playButton.backgroundColor = Color.BLACK;
+        playButton.borderColor = Color.BLACK;
+        playButton.borderRadius = 10;
+        playButton.setPadding(new Vec2(50, 10));
+        playButton.font = "PixelSimple";
+        playButton.onClick = () => {
+            console.log("Activated Help Butto");
+            this.createHelpMenu();
+        }
+        return playButton;
+
+    }
+
+    makeSettingButton():Button{
+        let midpoint = this.viewport.getCenter();
+        let playButton = <Button> this.add.uiElement(UIElementType.BUTTON,"mainMenu",{ position:new Vec2(midpoint.x,midpoint.y+120),text:"Setting"});
+        playButton.backgroundColor = Color.BLACK;
+        playButton.borderColor = Color.BLACK;
+        playButton.borderRadius = 10;
+        playButton.setPadding(new Vec2(50, 10));
+        playButton.font = "PixelSimple";
+        playButton.onClick = () => {
+            this.createSettingMenu();
+        }
+        return playButton;
+    }
+
+    createSettingMenu(){
+        this.setting = true;
+        let center = this.viewport.getCenter();
+        let settingBackground = <Rect>this.add.graphic(GraphicType.RECT,"settingMenuBackGround",{position:new Vec2(center.x,center.y),size:new Vec2(1000,500)});
+        settingBackground.color = Color.WHITE;
+
+        let musicSliderLabel = <UIElement> this.add.uiElement(UIElementType.LABEL,"settingMenu",{position: new Vec2(center.x-400,center.y),text:"muusic"});
+        let musicSlider = <UIElement> this.add.uiElement(UIElementType.SLIDER,"settingMenu",{position: new Vec2(center.x-100,center.y)});
+        musicSlider.backgroundColor = Color.GREEN; //there might be a bug with the slider, idk
+        musicSliderLabel.backgroundColor=Color.BLUE;
+
+        let exitButton = <UIElement> this.add.uiElement(UIElementType.BUTTON,"settingMenu",{position:new Vec2(center.x,center.y+200),text:"exit"});
+        exitButton.onClick = () =>{
+            this.setting=false;
+            musicSlider.destroy();
+            exitButton.destroy();
+            musicSliderLabel.destroy();
+            settingBackground.destroy();
+            console.log("Exit Setting");
+        }
+
+    }
+
+    createHelpMenu(){
+        this.setting = true;
+        let center = this.viewport.getCenter();
+        let settingBackground = <Rect>this.add.graphic(GraphicType.RECT,"settingMenuBackGround",{position:new Vec2(center.x,center.y),size:new Vec2(1000,500)});
+        settingBackground.color = Color.WHITE;
+
+        let helpTitle = <UIElement> this.add.uiElement(UIElementType.LABEL,"settingMenu", {position: new Vec2(center.x, center.y-200), text:"Help"});
+        let helptext = "Click to shoot fireballs, placeholder Text...";
+        let helpBody = <UIElement> this.add.uiElement(UIElementType.LABEL,"settingMenu",{position: new Vec2(center.x,center.y),text:helptext});
+
+        let exitButton = <UIElement> this.add.uiElement(UIElementType.BUTTON,"settingMenu",{position:new Vec2(center.x,center.y+200),text:"exit"});
+        exitButton.onClick = () =>{
+            this.setting=false;
+            helpTitle.destroy();
+            exitButton.destroy();
+            helpBody.destroy();
+            settingBackground.destroy();
+            console.log("Exit Setting");
+        }
     }
 }
