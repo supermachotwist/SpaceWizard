40a0520b771a8d31aea451b612bf1a3150ae0b76
EliTheCompSciGuy <35388026+EliTheCompSciGuy@users.noreply.github.com>
2021-05-03 12:49:32 -0400

    Add ts files for the new enemy types

---
+++ GameFiles/src/space_wizard/GameSystems/Enemys/EnemyTypes/Disruptor.ts
@@ -0,0 +1,12 @@
+import EnemyType from "../EnemyType";
+
+export default class disruptor extends EnemyType {
+
+    constructor(){
+        super();
+        this.displayName = "disruptor";
+        this.cooldown = 0;
+        this.speed = 0;
+        this.health = 30;
+    }
+}
---
+++ GameFiles/src/space_wizard/GameSystems/Enemys/EnemyTypes/SpikeEnemy.ts
@@ -0,0 +1,12 @@
+import EnemyType from "../EnemyType";
+
+export default class spikeEnemy extends EnemyType {
+
+    constructor(){
+        super();
+        this.displayName = "spikeEnemy";
+        this.cooldown = 100;
+        this.speed = 20;
+        this.health = 100;
+    }
+}
